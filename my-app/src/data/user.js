b81f50cc9f05a720a87b7325caf9eec3c7067ab6
Ali Mohammad <engalimohammad90@gmail.com>
2023-08-20 13:29:46 +0300

    Initial commit

---
+++ my-app/src/data/user.js
@@ -0,0 +1,97 @@
+
+const INFO = {
+	main: {
+
+	
+		
+
+		title: "Ali Mohammad portofolio",
+		name: "Ali Mohammad",
+		email: "eng.alimoh98m@gmail.com",
+     
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/aliModdd98",
+		linkedin: "https://www.linkedin.com/in/ali-mohammad-60a9b4286/",
+		instagram: "@3li_moddd",
+		stackoverflow: "https://stackoverflow.com/users/22326977/ali-mohammad",
+		facebook: "https://www.facebook.com/profile.php?id=100009507318769&mibextid=ZbWKwL",
+	},
+
+	homepage: {
+		title: "Front-end Developer and flutter developer previously.",
+		description:
+			"I am a Front-end developer with little expertise in React.js. As a software engineer with a passion for problem-solving and team collaboration, I am seeking a role as a frontend developer. With expertise in various	programming languages and experience with Agile	methodology, I am confident in my ability to create	innovative solutions for complex problems. Outside of work, my hobbies of exploring new places,reading, photography, and sports fuel my creativity and determination to succeed in any task",
+	},
+
+	about: {
+		title: "I’m Ali Mohammad ,I live in lattkia (Syria), where I design the future.",
+		description:
+			"I've worked on few projects in recent years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+		{
+			title: "Project 6",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		}
+	],
+};
+
+export default INFO;
